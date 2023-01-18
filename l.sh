eff3876809805a9493a75f2118a6155115d1bd27
2573874409@qq.com <z18344203426@qq.com>
2023-01-18 15:18:03 +0800

    优化

--- l.sh
+++ l.sh
@@ -15,50 +15,98 @@ ls
 cd ./CFPAOrg-Minecraft-Mod-Language-Package-${targetcommitish}
 cd ./projects
 ######################################################
-###################### 1.12.2
+######################### 1.12.2-es
 cd ./1.12.2
 cd ./assets
-tree -H 1.12.2（${name}） > 1.12.2.html
+tree -L 2 -H 000 1.12.2（${name}_以下显示的是文件夹，里面的文件请前往详细列表） > 1.12.2-es.html
 mv *.html ../
 cd ../
 mv *.html ../
 cd ../
-###################### 1.16
+#################### 1.12.2
+cd ./1.12.2
+cd ./assets
+tree -H 000 1.12.2（${name}_以下显示的是文件夹，里面的文件请前往详细列表） > 1.12.2.html
+mv *.html ../
+cd ../
+mv *.html ../
+cd ../
+######################### 1.16-es
 cd ./1.16
 cd ./assets
-tree -H 1.16（${name}） > 1.16.html
+tree -L 2 -H 000 1.16（${name}_以下显示的是文件夹，里面的文件请前往详细列表） > 1.16-es.html
 mv *.html ../
 cd ../
 mv *.html ../
 cd ../
-###################### 1.16-fabric
+#################### 1.16
+cd ./1.16
+cd ./assets
+tree -H 000 1.16（${name}_以下显示的是文件夹，里面的文件请前往详细列表） > 1.16.html
+mv *.html ../
+cd ../
+mv *.html ../
+cd ../
+######################### 1.16-fabric-es
 cd ./1.16-fabric
 cd ./assets
-tree -H 1.16-fabric（${name}） > 1.16-fabric.html
+tree -L 2 -H 000 1.16-fabric（${name}_以下显示的是文件夹，里面的文件请前往详细列表） > 1.16-fabric-es.html
 mv *.html ../
 cd ../
 mv *.html ../
 cd ../
-###################### 1.18
+#################### 1.16-fabric
+cd ./1.16-fabric
+cd ./assets
+tree -H 000 1.16-fabric（${name}_以下显示的是文件夹，里面的文件请前往详细列表） > 1.16-fabric.html
+mv *.html ../
+cd ../
+mv *.html ../
+cd ../
+######################### 1.18-es
 cd ./1.18
 cd ./assets
-tree -H 1.18（${name}） > 1.18.html
+tree -L 2 -H 000 1.18（${name}_以下显示的是文件夹，里面的文件请前往详细列表） > 1.18-es.html
 mv *.html ../
 cd ../
 mv *.html ../
 cd ../
-###################### 1.18-fabric
+#################### 1.18
+cd ./1.18
+cd ./assets
+tree -H 000 1.18（${name}_以下显示的是文件夹，里面的文件请前往详细列表） > 1.18.html
+mv *.html ../
+cd ../
+mv *.html ../
+cd ../
+######################### 1.18-fabric-es
 cd ./1.18-fabric
 cd ./assets
-tree -H 1.18-fabric（${name}） > 1.18-fabric.html
+tree -L 2 -H 000 1.18-fabric（${name}_以下显示的是文件夹，里面的文件请前往详细列表） > 1.18-fabric-es.html
 mv *.html ../
 cd ../
 mv *.html ../
 cd ../
-###################### 1.19
+#################### 1.18-fabric
+cd ./1.18-fabric
+cd ./assets
+tree -H 000 1.18-fabric（${name}_以下显示的是文件夹，里面的文件请前往详细列表） > 1.18-fabric.html
+mv *.html ../
+cd ../
+mv *.html ../
+cd ../
+######################### 1.19-es
 cd ./1.19
 cd ./assets
-tree -H 1.19（${name}） > 1.19.html
+tree -L 2 -H 000 1.19（${name}_以下显示的是文件夹，里面的文件请前往详细列表） > 1.19-es.html
+mv *.html ../
+cd ../
+mv *.html ../
+cd ../
+#################### 1.19
+cd ./1.19
+cd ./assets
+tree -H 000 1.19（${name}_以下显示的是文件夹，里面的文件请前往详细列表） > 1.19.html
 mv *.html ../
 cd ../
 mv *.html ../
